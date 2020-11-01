d1ea6c239d89a91c0509206568e2fe52256f82fb
Iskandar Mamazhanov <iskandarchik.1995@gmail.com>
2020-11-01 19:36:04 +0600

    Create class Iskandar

---
+++ src/main/java/com/example/blog/models/Iskandar.java
@@ -0,0 +1,11 @@
+package com.example.blog.models;
+
+public class Iskandar {
+    private int id;
+    private String name;
+
+    public Iskandar(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
